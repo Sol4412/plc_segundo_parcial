0ef5be250daf1ec73173416c017960892084276e
Sol4412 <143729643+Sol4412@users.noreply.github.com>
2023-09-27 22:57:17 -0600

    Create condicionales02.py

---
+++ condicionales02.py
@@ -0,0 +1,13 @@
+numero = float(input("Por favor, ingrese un número: "))
+if numero > 0:
+    print("El número ingresado es positivo.")
+elif numero < 0:
+    print("El número ingresado es negativo.")
+else:
+    print("El número ingresado es cero.")
+#
+n= float(input("Por favor, ingrese un número: "))
+if -10 <= n<= 10:
+    print("Hurra!")
+else:
+    print("El número no está en el rango de -10 a 10.")
